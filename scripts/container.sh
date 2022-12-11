99a72e775afef65ad5df943a22266cb142cfa2df
John Vishnefske <jvishnefske@gmail.com>
2022-12-11 09:43:22 -0500

    container script from siemens

--- scripts/container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-W=$(pwd)
-U=$(id -u):$(id -g)
-echo user: ${U}
-#IMAGE=docker.io/crops/poky
-IMAGE=ghcr.io/seimens/kas:latest-release
-#docker run -u ${U}  --rm -it -v $W:$W docker.io/crops/poky --workdir=${W}
-podman run  --volume yocto-workdir:/workdir --mount type=bind,source=${W},destination=${W},mode=777  -it --workdir ${W} ${IMAGE} $@
